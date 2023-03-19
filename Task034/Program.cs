3799e4adb1e1f6535bd2dc24594b3748b5dc0c4b
Anatoliy.Bykin <bykinlol@mail.ru>
2023-03-19 22:43:47 +0300

    add new Homework

---
+++ Task034/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+int size = GetNumberUser("Введите размер массива: ", "Ошибка ввода!");
+
+int[] array = GetArray(size, 100, 999);
+Console.WriteLine(String.Join(" ", array));
+
+int evenNumbers = GetEvenNumbers(array);
+Console.WriteLine($"Колличество четных чисел = {evenNumbers}");
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+
+int GetNumberUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+
+}
+
+int GetEvenNumbers(int[] arr)
+{
+
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) count++;
+    }
+    return count;
+
+
+}
